58436287d3bbbae3793d93d5dc8af6d0273af0a0
Martin Ekerå <ekera@kth.se>
2021-08-11 17:24:47 +0200

    Minor improvements to the source code documentation

--- src/tau_estimate.h
+++ src/tau_estimate.h
@@ -26,9 +26,9 @@
  * \brief   Computes tau_d and tau_r from n argument pairs (alpha_d, alpha_r)
  *          sampled from the distribution.
  * 
- * If at least one of the n argument pairs sampled is out of bounds, tau_d
- * and tau_r are both set to MAX_DBL and #FALSE is returned. Otherwise,
- * tau_d and tau_r are valid estimates and #TRUE is returned.
+ * If at least one of the n argument pairs sampled is out of bounds, tau_d and 
+ * tau_r are both set to MAX_DBL and #FALSE is returned. Otherwise, tau_d and 
+ * tau_r are valid estimates and #TRUE is returned.
  *
  * For x in {d, r}, we define tau_x as
  * 
@@ -37,7 +37,8 @@
  * To compute the volume quotients v_d and v_r, you may convert the output from
  * this function into volume quotients using tau_volume_quotient().
  *
- * \param[in] distribution      The distribution from which to sample pairs.
+ * \param[in] distribution      The distribution from which to sample argument 
+ *                              pairs (alpha_d, alpha_r).
  * \param[in, out] random_state The random state to use when sampling.
  * \param[in] n                 The integer n.
  * \param[out] tau_d            The computed value of tau_d.
@@ -56,9 +57,9 @@ bool tau_estimate(
 /*!
  * \brief   Computes tau from n arguments alpha sampled from the distribution.
  *
- * If at least one of the n arguments alpha sampled is out of bounds, tau
- * is set to MAX_DBL and #FALSE is returned. Otherwise, tau is a valid estimate
- * and #TRUE is returned.
+ * If at least one of the n arguments alpha sampled is out of bounds, tau is set
+ * to MAX_DBL and #FALSE is returned. Otherwise, tau is a valid estimate and 
+ * #TRUE is returned.
  *
  * We define tau = log_( (1 / n) sum_{i = 1}^{n} alpha^2_i ) / 2 - m.
  * 
