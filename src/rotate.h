9d1d3ca073ab30deaac0521c5d8997773272eefb
Martin Ekerå <ekera@kth.se>
2026-05-03 15:42:57 +0200

    Removed semicolons

--- src/rotate.h
+++ src/rotate.h
@@ -11,11 +11,11 @@
 /*!
  * \brief   Rotates a 64 bit integer s steps to the left cyclically.
  */
-#define ROTATE_LEFT_UINT64(x, s) (((x) << (s)) | ((x) >> (64 - (s))));
+#define ROTATE_LEFT_UINT64(x, s) (((x) << (s)) | ((x) >> (64 - (s))))
 
 /*!
  * \brief   Rotates a 64 bit integer s steps to the right cyclically.
  */
-#define ROTATE_RIGHT_UINT64(x, s) (((x) >> (s)) | ((x) << (64 - (s))));
+#define ROTATE_RIGHT_UINT64(x, s) (((x) >> (s)) | ((x) << (64 - (s))))
 
 #endif /* ROTATE_H */
